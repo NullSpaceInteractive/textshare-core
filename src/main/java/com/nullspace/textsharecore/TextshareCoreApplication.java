9fabaf1348d1bba6bff4f9ef5c113aedb6109443
Anthony <pcmasterrace0805@gmail.com>
2021-01-26 22:10:57 -0600

    Initial commit

---
+++ src/main/java/com/nullspace/textsharecore/TextshareCoreApplication.java
@@ -0,0 +1,13 @@
+package com.nullspace.textsharecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextshareCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextshareCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nullspace/textsharecore/TextshareCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullspace.textsharecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextshareCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
